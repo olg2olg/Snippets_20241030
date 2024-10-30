9a2e71a3c41bfb668643c81e115cd3c4678c43fc
olg2olg <olga17573@gmail.com>
2024-10-30 23:15:18 +0300

    Day3.Snippets:task0-4

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('', views.index_page, name='index-page'),
+    path('snippets/add', views.add_snippet_page, name='add-snippet-page'),
+    path('snippets/list', views.snippets_page, name='snippets-page'),
+    #path("articles/<int:year>/", views.year_archive, name="news-year-archive"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
