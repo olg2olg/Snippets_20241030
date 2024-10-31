1fbb7978113ee71856a4a840a8c1c323fe7208ef
olg2olg <olga17573@gmail.com>
2024-10-31 13:31:55 +0300

    Day4.Start django form

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
-from django.http import Http404, HttpResponseNotFound
+from django.http import Http404, HttpResponse #HttpResponseNotFound
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
+from MainApp.forms import SnippetForm
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -9,7 +10,11 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {
+        'pagename': 'Добавление нового сниппета',
+        'form': form 
+        }
     return render(request, 'pages/add_snippet.html', context)
 
 
@@ -35,3 +40,17 @@ def snippet_detail(request, snippet_id: int):
     #     }
     context['snippet'] = snippet
     return render(request, 'pages/snippet_detail.html', context) 
+
+
+def create_snippet(request):
+    #если в запросе метод POST, то распечатает значения
+    #<QueryDict: {'csrfmiddlewaretoken': ['vRxqcWoOa4fXwxHZnu1H6Qtrn4mKGN54jefQ8Jqewc2TKcjYo65tpt2ro5yZWDyA'], 
+    # 'name': ['third'], 'lang': ['cpp'], 'code': ['cout']}>
+    #[31/Oct/2024 13:26:12] "POST /snippets/create HTTP/1.1" 200 4
+    from pprint import pprint
+
+    if request.method == "POST":
+        pprint(request.POST)
+        return HttpResponse("Done")
+
+
