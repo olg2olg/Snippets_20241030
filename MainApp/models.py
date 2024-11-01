a5e95a1432a43c520c79a99462004549eeae3a85
olg2olg <olga17573@gmail.com>
2024-11-01 16:47:30 +0300

    Day5_3. Add admin site

--- MainApp/models.py
+++ MainApp/models.py
@@ -19,11 +19,17 @@ class Snippet(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     is_public=models.BooleanField(default=True) #True = public, False = private
 
-    def __repr(self) -> str:
+    def __repr__(self) -> str:
         return f"Snippet({self.name}, {self.lang})"
+    
+    def __str__(self):
+        return f'{self.name}'
 
 class Comment(models.Model):
     text = models.TextField(max_length=1000)
     creation_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(to=User,on_delete=models.CASCADE)
     snippet = models.ForeignKey(to=Snippet, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'{self.text}'
